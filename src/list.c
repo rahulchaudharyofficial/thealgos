71b6294122a435f5651f1e0afb7a0ea870bcf979
Rahul Chaudhary <rahulchaudharyofficial@outlook.com>
2021-04-17 03:01:03 +0530

    more changes

--- src/list.c
+++ src/list.c
@@ -1,14 +1,62 @@
 #include "list.h"
 #include "allocator.h"
+#include "logging.h"
+
+const char *required_param_message = "Required param is null\n";
+const char *memory_allocation_message = "Failed to allocate memory when creating list\n";
 
 node_t* create_list()
 {
-    return NULL;
+    node_t *head = (node_t*) malloc(sizeof(node_t));
+    if(head)
+    {
+        int size = 0;
+        int *ptr = &size;
+        head->value = ptr;
+        head->next = NULL;
+        head->previous = NULL;
+    }
+    else{
+        log_message(COLOR_RED,memory_allocation_message);
+    }
+    return head;
 }
 
 node_t* add_node(node_t* head,void* value)
 {
-    return NULL;
+    if(head)
+    {
+        node_t *node = create_node(value);
+        if(node)
+        {
+            int *result_ptr = (int*) (head->value);
+            *result_ptr = *result_ptr + 1;
+            if(*result_ptr == 0 || head->next == NULL) //Add first node
+            {
+                head->next = node;
+                head->previous=node;
+            }
+            else //Add node at the end
+            {
+                node_t *temp = head;
+                while(temp->next!=NULL)
+                {
+                    temp = temp->next;
+                }
+                temp->next = node;
+                node->previous = temp;
+                head->previous = node;
+            }
+        }
+        else
+        {
+            log_message(COLOR_RED,memory_allocation_message);
+        }
+    }
+    else{
+        log_message(COLOR_RED,required_param_message);
+    }
+    return head;
 }
 
 node_t* remove_node(node_t* head,node_t* node)
@@ -25,6 +73,3 @@ void travere(node_t* head)
 {
 
 }
-
-
-
